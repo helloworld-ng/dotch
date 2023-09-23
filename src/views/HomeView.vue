bb19f369c3334ecdb1d5ae7273ad6622b5d0ec83
Opemipo Aikomo <opemipoaikomo@gmail.com>
2023-09-23 21:04:13 +0100

    Add homepage content

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,20 @@ import CameraMan from '../components/CameraMan.vue'
 </script>
 
 <template>
+  <header>
+    <img alt="Dotch logo" src="../assets/logo.svg" />
+  </header>
   <main>
     <CameraMan />
   </main>
+  <footer>
+    <div id="default">
+      <div><a href="#">About</a></div>
+      <div class="divider"></div>
+      <div><a href="#">Help</a></div>
+    </div>
+    <div id="about">
+      <button>Close</button>
+    </div>
+  </footer>
 </template>
